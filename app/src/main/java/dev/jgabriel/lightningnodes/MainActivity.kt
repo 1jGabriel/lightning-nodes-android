96de6215a6ce678847df0dead4c69aec815f3b35
1jgabriel <gsantana152@gmail.com>
2025-02-23 19:53:45 -0300

    Fix dependecies and imports

--- app/src/main/java/dev/jgabriel/lightningnodes/MainActivity.kt
+++ app/src/main/java/dev/jgabriel/lightningnodes/MainActivity.kt
@@ -4,25 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dev.jgabriel.lightningnodes.data.client.provideLightningApi
-import dev.jgabriel.lightningnodes.data.client.provideOkHttpClient
-import dev.jgabriel.lightningnodes.data.client.provideRetrofit
-import dev.jgabriel.lightningnodes.data.repository.LightningNodesRepositoryImpl
-import dev.jgabriel.lightningnodes.ui.theme.LightningNodesTheme
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import dev.jgabriel.design.components.ErrorScreen
+import dev.jgabriel.design.components.Loading
+import dev.jgabriel.design.theme.LightningNodesTheme
+import dev.jgabriel.presentation.LightningNodesViewModel
+import org.koin.compose.viewmodel.koinViewModel
 
 class MainActivity : ComponentActivity() {
-    val repository =
-        LightningNodesRepositoryImpl(api = provideLightningApi(provideRetrofit(provideOkHttpClient())))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,36 +34,115 @@ class MainActivity : ComponentActivity() {
         setContent {
             LightningNodesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val coroutineScope = rememberCoroutineScope()
-                    LaunchedEffect(Unit) {
-                        coroutineScope.launch {
-                            val result = repository.getNodes()
-                            println(result)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = MaterialTheme.colorScheme.background)
+                            .padding(innerPadding)
+                    ) {
+                        val viewModel = koinViewModel<LightningNodesViewModel>()
+                        val state by viewModel.state.collectAsStateWithLifecycle()
+                        if (state.isLoading) {
+                            Loading()
+                        }
+                        if (state.showError) {
+                            ErrorScreen(
+                                onClickButton = viewModel::getLightningNodes,
+                                title = "Ops, tivemos um erro.",
+                                description = "Não foi possível carregar os dados, tente novamente clicando abaixo"
+                            )
+                        }
+                        if (state.nodesList.isNotEmpty()) {
+                            LazyColumn {
+                                item {
+                                    Text(
+                                        modifier = Modifier.padding(16.dp),
+                                        text = "Nós Lightning",
+                                        style = MaterialTheme.typography.headlineLarge,
+                                        color = MaterialTheme.colorScheme.onBackground
+                                    )
+                                }
+                                items(
+                                    items = state.nodesList,
+                                    key = { it.publicKey }) {
+                                    Card(
+                                        modifier = Modifier
+                                            .padding(16.dp)
+                                            .fillMaxWidth(),
+                                        shape = ShapeDefaults.Medium,
+                                    ) {
+                                        Text(
+                                            modifier = Modifier.padding(8.dp),
+                                            text = it.alias,
+                                            style = MaterialTheme.typography.titleLarge,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+                                        Text(
+                                            modifier = Modifier.padding(
+                                                vertical = 4.dp,
+                                                horizontal = 8.dp
+                                            ),
+                                            text = it.capacity,
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+                                        Text(
+                                            modifier = Modifier.padding(
+                                                vertical = 4.dp,
+                                                horizontal = 8.dp
+                                            ),
+                                            text = (it.city?.ptBR
+                                                ?: it.city?.en).orEmpty() + (it.country?.ptBR
+                                                ?: it.country?.en).orEmpty(),
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+
+                                        Text(
+                                            modifier = Modifier.padding(
+                                                vertical = 4.dp,
+                                                horizontal = 8.dp
+                                            ),
+                                            text = "Canais: " + it.channels,
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+
+                                        Text(
+                                            modifier = Modifier.padding(
+                                                vertical = 4.dp,
+                                                horizontal = 8.dp
+                                            ),
+                                            text = it.publicKey,
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+
+                                        Text(
+                                            modifier = Modifier.padding(
+                                                vertical = 4.dp,
+                                                horizontal = 8.dp
+                                            ),
+                                            text = "Visto pela primeira vez: " + it.firstSeen,
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+                                        Text(
+                                            modifier = Modifier.padding(
+                                                vertical = 4.dp,
+                                                horizontal = 8.dp
+                                            ),
+                                            text = "Última atualização: " + it.updatedAt,
+                                            style = MaterialTheme.typography.bodyMedium,
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+                                    }
+                                }
+                            }
                         }
                     }
-
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LightningNodesTheme {
-        Greeting("Android")
     }
 }
