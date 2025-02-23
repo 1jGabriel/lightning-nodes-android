15f280a4b70f816b9275cfbe5050f31ff6ab45a8
1jgabriel <gsantana152@gmail.com>
2025-02-23 20:47:34 -0300

    added button to refresh

--- app/src/main/java/dev/jgabriel/lightningnodes/MainActivity.kt
+++ app/src/main/java/dev/jgabriel/lightningnodes/MainActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -52,15 +60,29 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         if (state.nodesList.isNotEmpty()) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        viewModel.getLightningNodes()
+                                    }
+                                    .padding(16.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .weight(1f),
+                                    text = stringResource(R.string.lightning_nodes_title),
+                                    style = MaterialTheme.typography.headlineLarge,
+                                    color = MaterialTheme.colorScheme.onBackground
+                                )
+                                Image(
+                                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                                    imageVector = Icons.Filled.Refresh,
+                                    contentDescription = null
+                                )
+                            }
                             LazyColumn {
-                                item {
-                                    Text(
-                                        modifier = Modifier.padding(16.dp),
-                                        text = stringResource(R.string.lightning_nodes_title),
-                                        style = MaterialTheme.typography.headlineLarge,
-                                        color = MaterialTheme.colorScheme.onBackground
-                                    )
-                                }
                                 items(
                                     items = state.nodesList,
                                     key = { it.publicKey }) {
